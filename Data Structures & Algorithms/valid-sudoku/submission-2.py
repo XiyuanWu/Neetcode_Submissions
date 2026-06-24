1bdcbb8048137b8bf2593176b28d89e0f86aa591
Xiyuan Wu <xiyuan.24706826@gmail.com>
2026-06-24 15:23:54 -0700

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row = set()
+        col = set()
+        box = set()
+
+        for r in range(9):
+            for c in range(9):
+                num = board[r][c]
+
+                if num == ".":
+                    continue
+
+                row_key = (r, num)
+                col_key = (c, num)
+                box_key = (r // 3, c // 3, num)
+
+                if row_key in row or col_key in col or box_key in box:
+                    return False
+
+                row.add(row_key)
+                col.add(col_key)
+                box.add(box_key)
+            
+        return True
