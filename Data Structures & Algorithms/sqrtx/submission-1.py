daae376d87e74b78540f1f27d1f3aedf67b33c05
Xiyuan Wu <xwu136@gmail.com>
2026-07-06 22:56:33 -0700

    Add: sqrtx - submission-1

---
+++ Data Structures & Algorithms/sqrtx/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l, r = 0, x
+
+        while l <= r:
+            mid = (l + r) // 2
+            square = mid * mid
+
+            if square == x:
+                return mid
+            elif square < x:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return r
