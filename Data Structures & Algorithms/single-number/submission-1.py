6d5f2f6bcb1163bc9eefed4a0a1a11dc3f6ec8ea
Xiyuan Wu <xwu136@gmail.com>
2026-07-28 10:24:44 -0700

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        count = defaultdict(int)
+
+        for i in nums:
+            count[i] += 1
+
+        for num, freq in count.items():
+            if freq == 1:
+                return num
