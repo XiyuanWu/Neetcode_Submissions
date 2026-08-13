b1f1b141b0f28a65c6b0ebbef979b71efc298038
Xiyuan Wu <xwu136@gmail.com>
2026-08-13 16:27:31 -0700

    Add: island-perimeter - submission-3

---
+++ Data Structures & Algorithms/island-perimeter/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def islandPerimeter(self, matrix: List[List[int]]) -> int:
+        if not matrix: return 0
+
+        rows, cols = len(matrix), len(matrix[0])
+        dir = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        result = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if matrix[r][c] == 1:
+                    
+                    for dr, dc in dir:
+                        nr, nc = r + dr, c + dc
+
+                        if nr not in range(rows) or nc not in range(cols) or matrix[nr][nc] == 0:
+                            result += 1
+
+        return result
+                    
