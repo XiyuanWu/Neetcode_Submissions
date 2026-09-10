0d81d7d8bd0750a0532cf7ae272cc58419d9af4c
Xiyuan Wu <xwu136@gmail.com>
2026-09-09 21:25:49 -0700

    Add: design-twitter-feed - submission-1

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-1.py
@@ -0,0 +1,27 @@
+class Twitter:
+
+    def __init__(self):
+        self.time = 0
+        self.tweets = defaultdict(list)
+        self.follows = defaultdict(set)
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.tweets[userId].append([self.time, tweetId])
+        self.time += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        users = {userId} | self.follows[userId]
+
+        all_tweets = []
+        for user in users:
+            all_tweets += self.tweets[user]
+
+        all_tweets.sort(key=lambda x: x[0], reverse=True)
+
+        return [tweetId for time, tweetId in all_tweets[:10]]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.follows[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.follows[followerId].discard(followeeId)
